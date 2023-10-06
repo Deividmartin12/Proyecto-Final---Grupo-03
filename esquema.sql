fd3652dcea126d6b1f85301472c8bbf32fdc5d45
Deividmartin12 <131992849+Deividmartin12@users.noreply.github.com>
2023-10-05 19:17:37 -0500

    bd

---
+++ esquema.sql
@@ -0,0 +1,62 @@
+create table cliente(
+	cliente_id serial primary key,
+    nombre varchar(255) not null,
+    email varchar(255) not null,
+    telefono varchar(20) not null,
+    direccion varchar(255) not null,
+    cliente_dni char(8) unique not null,
+    password varchar(255) not null
+);
+
+create table categoria(
+	categoria_id serial primary key,
+    nombre varchar(255) not null,
+    descripcion varchar(255) not null
+);
+
+create table producto(
+	producto_id serial primary key,
+    nombre varchar(255) not null,
+    descipcion varchar(255) not null,
+    precio numeric(8,2) not null,
+    stock integer not null,
+    estado boolean not null,
+    categoria_id integer not null,
+    foreign key categoria_id references categoria(categoria_id)
+);
+ 
+
+create table pedido(
+	pedido_id serial primary key,
+    fecha_pedido date not null,
+    estado_pedido char(1) not null,
+    cliente_id integer not null,
+    foreign key cliente_id references clientes(cliente_id)
+);
+
+create table detalle_pedido(
+	pedido_id integer not null,
+    producto_id integer not null,
+    cantidad integer not null,
+    precio_unitario numeric(8,2) not null,
+    primary key (pedido_id, producto_id),
+    foreign key(pedido_id) references pedido(pedido_id),
+    foreign key(producto_id) references producto(producto_id)
+);
+
+create table metodo_pago(
+	metodo_id serial not null,
+    nombre varchar(255) not null unique,
+    descripcion varchar(255) not null
+);
+
+create table comprobante(
+	numero_boleta integer primary key,
+    fecha_hora_emision timestamp not null,
+    monto_total numeric(8,2) not null,
+    tipo_comprobante char(1) not null,
+    pedido_id integer not null,
+    metodo_id integer not null,
+    foreign key pedido_id references pedido(pedido_id),
+    foreign key metodo_id references metodo_pago(metodo_id)
+);
