28c819384e9b27e5356a3b0ce15a75b9442eb7dd
Deividmartin12 <131992849+Deividmartin12@users.noreply.github.com>
2023-10-18 20:02:39 -0500

    cornejo

--- controladores/controlador_metodoP.py
+++ controladores/controlador_metodoP.py
@@ -1,7 +1,7 @@
-from bd import obtener_conexion
+from bd import obtenerConexion
 
 def insertar_metodo_pago(nombre, descripcion):
-    conexion = obtener_conexion()
+    conexion = obtenerConexion()
     with conexion.cursor() as cursor:
         cursor.execute(
             "INSERT INTO metodo_pago(nombre, descripcion) VALUES (%s, %s)",
@@ -11,7 +11,7 @@ def insertar_metodo_pago(nombre, descripcion):
     conexion.close()
 
 def obtener_metodos_pago():
-    conexion = obtener_conexion()
+    conexion = obtenerConexion()
     metodos_pago = []
     with conexion.cursor() as cursor:
         cursor.execute("SELECT metodo_id, nombre, descripcion FROM metodo_pago")
@@ -20,14 +20,14 @@ def obtener_metodos_pago():
     return metodos_pago
 
 def eliminar_metodo_pago(id):
-    conexion = obtener_conexion()
+    conexion = obtenerConexion()
     with conexion.cursor() as cursor:
         cursor.execute("DELETE FROM metodo_pago WHERE metodo_id = %s", (id,))
     conexion.commit()
     conexion.close()
 
 def obtener_metodo_pago_por_id(id):
-    conexion = obtener_conexion()
+    conexion = obtenerConexion()
     metodo_pago = None
     with conexion.cursor() as cursor:
         cursor.execute(
@@ -37,7 +37,7 @@ def obtener_metodo_pago_por_id(id):
     return metodo_pago
 
 def actualizar_metodo_pago(nombre, descripcion, id):
-    conexion = obtener_conexion()
+    conexion = obtenerConexion()
     with conexion.cursor() as cursor:
         cursor.execute(
             "UPDATE metodo_pago SET nombre = %s, descripcion = %s WHERE metodo_id = %s",
