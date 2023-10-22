a5588d5d6c740dbb78713be7514fc3b26ff13e2a
David <caballerovasquezdavidantonio@gmail.com>
2023-10-22 12:24:01 -0500

    Caballero

--- bd.py
+++ bd.py
@@ -1,7 +1,12 @@
 import pymysql
 
 def obtenerConexion():
-    return pymysql.connect(host='grupo1DAW.mysql.pythonanywhere-services.com',
+    """ return pymysql.connect(host='grupo1DAW.mysql.pythonanywhere-services.com',
                            user='grupo1DAW',
                            password='losjala2',
-                           bd='grupo1DAW$default')
+                           db='grupo1DAW$default') """
+    return pymysql.connect(host='127.0.0.1',
+                           port=3306,
+                           user='root',
+                           password='',
+                           db='project_veterinaria')
