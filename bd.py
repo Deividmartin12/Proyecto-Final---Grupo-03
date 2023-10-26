76c770111c4cc0bb49bb5a2cd9393c18d383c49d
David <caballerovasquezdavidantonio@gmail.com>
2023-10-26 15:57:24 -0500

    Caballero

--- bd.py
+++ bd.py
@@ -5,3 +5,9 @@ def obtenerConexion():
                            user='grupo1DAW',
                            password='losjala2',
                            db='grupo1DAW$default')
+
+def obtenerConexion():
+    return pymysql.connect(host='127.0.0.1',
+                           user='root',
+                           password='',
+                           db='project_veterinaria')
