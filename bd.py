dca5563eb8550658ea1a768733ba21d7dfdaa2bd
Deividmartin12 <131992849+Deividmartin12@users.noreply.github.com>
2023-11-24 08:07:50 -0500

    Delete bd.py

--- bd.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-
-def obtenerConexion():
-    return pymysql.connect(host='grupo1DAW.mysql.pythonanywhere-services.com',
-                           user='grupo1DAW',
-                           password='losjala2',
-                           db='grupo1DAW$default')
