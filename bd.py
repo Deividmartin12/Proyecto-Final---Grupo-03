0c245fb72813d82c7e2ab00e95a5c84a7ae62a13
dasalos <dasalos@hotmail.com>
2023-11-30 23:03:23 -0500

    d

--- bd.py
+++ bd.py
@@ -1,15 +1,15 @@
 import pymysql
 
+# def obtenerConexion():
+#     return pymysql.connect(host='127.0.0.1',
+#                            user='root',
+#                            password='',
+#                            db='project_veterinaria')
+
+
+
 def obtenerConexion():
-    return pymysql.connect(host='127.0.0.1',
-                           user='root',
-                           password='',
-                           db='project_veterinaria')
-
-
-
-#def obtenerConexion():
- #   return pymysql.connect(host='grupo1DAW.mysql.pythonanywhere-services.com',
-  #                         user='grupo1DAW',
-   #                        password='losjala2',
-      #                     db='grupo1DAW$default')
+   return pymysql.connect(host='grupo1DAW.mysql.pythonanywhere-services.com',
+                          user='grupo1DAW',
+                          password='losjala2',
+                          db='grupo1DAW$default')
